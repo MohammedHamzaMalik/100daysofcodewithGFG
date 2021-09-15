756183dc8dd35522ad63cc23fc59f1c275d929e2
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-09-15 22:39:55 +0530

    Explanation added

--- 1 - Day/main.py
+++ 1 - Day/main.py
@@ -26,3 +26,17 @@ if __name__ == '__main__':
         print()
 
 # } Driver Code Ends
+
+'''
+Explanation:
+In Python there is a built-in method list.sort(), which will sort a list simple ascending order.
+There is also a built-in function sorted(list) which will return a new sorted list.
+The difference between both of them is that the list.sort() method is only defined for lists, whereas the sorted() function accepts any iterable.
+
+For sorting in descending order add a reverse parameter with a boolean value (reverse=True).
+
+Key Functions can also be used to sort according to the key. 
+Both list.sort() and sorted() have a key parameter to specify a function (or other callable) to be called on each list element prior to making comparisons.
+
+For more information check out the the documentation: https://docs.python.org/3/howto/sorting.html
+'''
