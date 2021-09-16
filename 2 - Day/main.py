745fc948c3c6ed6e0c68bff703ba5d25f3bd623b
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-09-16 23:52:25 +0530

    Complete explanation added

--- 2 - Day/main.py
+++ 2 - Day/main.py
@@ -57,3 +57,19 @@ if __name__ == '__main__':
         ob=Solution()
         print(ob.maximumMeetings(n,start,end))
 # } Driver Code Ends
+
+'''
+Explanation:
+To solve this problem I have used the greedy approah.
+1 -> first I have taken the start and end time of every meeting and added it in a total_meetings 
+     array.
+2 -> second I have sorted the total_meetings array with a key of second element i.e. end time. 
+     I have sorted using the similar method which I have used in the day 1 problem.
+3 -> now I have add the first elements position of total_meetings to a new array final_meetings
+4 -> then I have taken the end time of first element of total_meetings and assigned it to the 
+     variable time_limit
+5 -> now I have iterated over second element to the last element of total_meetings & checked that
+     if the start time of upcoming element is greater than the end time of previous element than 
+     added that element's position to the final_meetings and updated the end time.
+6 -> and at last I have returned the length of final_meetings which will be the answer for this 
+     problem.
