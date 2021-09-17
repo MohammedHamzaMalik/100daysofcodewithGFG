c77fd2fc369fec648e0ba8d94893da5aa96ac595
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-09-17 22:56:41 +0530

    Explanation added

--- 3 - Day/main.py
+++ 3 - Day/main.py
@@ -1,3 +1,8 @@
+'''
+Problem: K-th element of two sorted Arrays
+Given two sorted arrays arr1 and arr2 of size N and M respectively and an element K. 
+The task is to find the element that would be at the k’th position of the final sorted array.
+'''
 #User function Template for python3
 
 class Solution:
@@ -38,3 +43,13 @@ if __name__ == "__main__":
     main()
 
 # } Driver Code Ends
+
+'''
+Explanation:
+In this I have simply used the for loops and if condition.
+1 -> first I have made a new array in which I will be storing the elements of both of the arrays
+2 -> then I have iterated over the first array and added of its element in the final array
+3 -> similarly I have done the same process on the second array
+4 -> then using sort built-in method I have sorted the final array (you can use any of the sorting method)
+5 -> then I have iterated over the final array and checked if the position of any of the element of the final array is eqaul to k. If yes then I have returned the element just before the position which is equal to k.  
+'''
