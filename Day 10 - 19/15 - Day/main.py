a821df1a3c732966d167dae10c894c447ffdc28b
Mohammed Hamza Malik <63325246+MohammedHamzaMalik@users.noreply.github.com>
2021-09-29 19:09:59 +0530

    code modified

--- Day 10 - 19/15 - Day/main.py
+++ Day 10 - 19/15 - Day/main.py
@@ -3,7 +3,30 @@
 class Solution: 
     def candies(self, m,n): 
         # Your code goes here 
-        ans=int(((m-1)*(n-1)/2))
+		# ans=int(((m-1)*(n-1)/2))
+		i = 0
+    	X = (m * n) - m - n
+
+    	queue = []
+    	queue.append(X)
+    	set = {X}
+
+    	ans = 0
+    	while (len(queue) > 0):
+    		curr = queue[0] 
+    		queue.remove(queue[0])
+
+    		ans += 1
+    		key = curr-m
+    		if (key > 0 and key not in set):
+    			queue.append(key)
+    			set.add(key)
+
+    		key = curr-n
+    		if (key > 0 and key not in set):
+    			queue.append(key)
+    			set.add(key)
+				
         return ans
 
 #{ 
